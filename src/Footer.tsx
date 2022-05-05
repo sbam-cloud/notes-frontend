a87fdba80785b9b65056e9a4eb31434c631ee1aa
Nicola Racco <nicola@mikamai.com>
2022-05-05 09:41:24 +0200

    social links in new tab

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -21,6 +21,7 @@ const SocialButton = ({
       cursor={'pointer'}
       as={'a'}
       href={href}
+      target='_blank'
       display={'inline-flex'}
       alignItems={'center'}
       justifyContent={'center'}
