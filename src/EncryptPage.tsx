36060c0c927d6e5ab6d7ad358f17f399edd5aaa3
Nicola Racco <nicola@mikamai.com>
2022-05-11 17:41:10 +0200

    styles for encrypt page

--- src/EncryptPage.tsx
+++ src/EncryptPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, FormControl, FormLabel, HStack, Input, Link, Stack, Textarea, Tooltip } from "@chakra-ui/react";
+import { Alert, Box, Button, Flex, FormControl, FormLabel, HStack, Input, Link, Stack, Textarea, Tooltip, VStack } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import { useEffect, useState } from "react";
 import { InternalError } from "./errors";
@@ -56,13 +56,25 @@ export default function EncryptPage() {
     <Box>
       {generatedUrl === null ? (
         <form onSubmit={formik.handleSubmit}>
-          <FormControl id='message'>
-            <FormLabel>Message</FormLabel>
-            <Textarea onChange={formik.handleChange} value={formik.values.message} />
-          </FormControl>
-          <HStack justify='flex-end' paddingTop={2} w='full'>
-            <Button type='submit'>Encrypt</Button>
-          </HStack>
+          <Box justifyContent='stretch' display={{ md: 'flex' }}>
+            <Box mx={{ md: 5 }} p={5} borderWidth={1} borderRadius='5px' w='auto' sx={{ gap: 25 }} whiteSpace={{ md: 'nowrap' }} order={{ md: 1 }}>
+              <p>Type the message you want to share and click <strong>Encrypt</strong>.</p>
+              <p>We store the encrypted message only and give you a<br/>URL containing the encryption key.</p>
+              <Alert status='warning'>Nobody can read your message<br/>without the encryption key.</Alert>
+              <p>Using the link you can decipher the message.</p>
+              <Alert status='warning'>It will self-destroy after opening it.</Alert>
+              <p>If the link is not used, the message expires after 7 days.</p>
+            </Box>
+            <Flex w='full' direction='column' mt={{ base: 5, md: 0 }}>
+              <FormControl id='message' h='full' as={Flex} flexDirection='column'>
+                <FormLabel>Message</FormLabel>
+                <Textarea onChange={formik.handleChange} value={formik.values.message} resize='vertical' h='full' minH={200} />
+              </FormControl>
+              <HStack justify='flex-end' paddingTop={2} w='full' h={100}>
+                <Button type='submit'>Encrypt</Button>
+              </HStack>
+            </Flex>
+          </Box>
         </form>
       ) : (
         <FormControl id='generatedUrl'>
