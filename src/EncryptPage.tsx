df557bd5c640952a95b887b41fd2b131dec4ac3d
Nicola Racco <nicola@mikamai.com>
2022-05-12 15:48:04 +0200

    fix styling

--- src/EncryptPage.tsx
+++ src/EncryptPage.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Button, Flex, FormControl, FormLabel, HStack, Input, Link, Stack, Textarea, Tooltip, VStack } from "@chakra-ui/react";
+import { Alert, Box, Button, Flex, FormControl, FormLabel, HStack, Input, Link, Spinner, Stack, Textarea, Tooltip, VStack } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import { useEffect, useState } from "react";
 import { InternalError } from "./errors";
@@ -52,44 +52,61 @@ export default function EncryptPage() {
     return <InternalError />;
   }
 
-  return (
-    <Box>
-      {generatedUrl === null ? (
-        <form onSubmit={formik.handleSubmit}>
-          <Box justifyContent='stretch' display={{ md: 'flex' }}>
-            <Box mx={{ md: 5 }} p={5} borderWidth={1} borderRadius='5px' w='auto' sx={{ gap: 25 }} whiteSpace={{ md: 'nowrap' }} order={{ md: 1 }}>
-              <p>Type the message you want to share and click <strong>Encrypt</strong>.</p>
-              <p>We store the encrypted message only and give you a<br/>URL containing the encryption key.</p>
-              <Alert status='warning'>Nobody can read your message<br/>without the encryption key.</Alert>
-              <p>Using the link you can decipher the message.</p>
-              <Alert status='warning'>It will self-destroy after opening it.</Alert>
-              <p>If the link is not used, the message expires after 7 days.</p>
-            </Box>
-            <Flex w='full' direction='column' mt={{ base: 5, md: 0 }}>
-              <FormControl id='message' h='full' as={Flex} flexDirection='column'>
-                <FormLabel>Message</FormLabel>
-                <Textarea onChange={formik.handleChange} value={formik.values.message} resize='vertical' h='full' minH={200} />
-              </FormControl>
-              <HStack justify='flex-end' paddingTop={2} w='full' h={100}>
-                <Button type='submit'>Encrypt</Button>
-              </HStack>
-            </Flex>
-          </Box>
-        </form>
-      ) : (
-        <FormControl id='generatedUrl'>
-          <FormLabel>Generated Link</FormLabel>
-          <HStack w='full'>
-            <Input readOnly value={generatedUrl} />
-            <Tooltip label="Link copied!" isOpen={urlCopiedToClipboard}>
-              <Button onClick={() => handleCopyToClipboard(generatedUrl)}>Copy</Button>
-            </Tooltip>
+  if (generatedUrl) {
+    return (
+      <Box justifyContent='stretch' display={{ md: 'flex' }}>
+        <Box marginLeft={{ md: 5 }} p={{ md: 5 }} sx={{ gap: 25 }} whiteSpace={{ md: 'nowrap' }} order={{ md: 1 }} lineHeight={2} fontSize='sm'>
+          <p>The message you encrypted can be read only by <strong>using the generated link</strong>.</p>
+          <Alert status='warning'>Remember: the message is destroyed once it has been read.</Alert>
+          <p>The message expires automatically after 7 days.</p>
+        </Box>
+        <Flex w='full' direction='column' mt={{ base: 5, md: 0 }}>
+          <FormControl id='generatedUrl'>
+            <FormLabel>Generated Link</FormLabel>
+            <HStack w='full'>
+              <Input readOnly value={generatedUrl} />
+              <Tooltip label="Link copied!" isOpen={urlCopiedToClipboard}>
+                <Button onClick={() => handleCopyToClipboard(generatedUrl)}>Copy</Button>
+              </Tooltip>
+            </HStack>
+          </FormControl>
+          <HStack justify='flex-end' paddingTop={2} w='full' h={100}>
+          <Link onClick={handleReset}>Share a new note!</Link>
           </HStack>
-          <Stack align='center' alignSelf='center' marginTop={5}>
-            <Link onClick={handleReset}>Share a new note!</Link>
-          </Stack>
-        </FormControl>
+        </Flex>
+      </Box>
+    )
+  }
+  return (
+    <Box justifyContent='stretch' display={{ md: 'flex' }}>
+      <Box marginLeft={{ md: 5 }} p={{ md: 5 }} sx={{ gap: 25 }} whiteSpace={{ md: 'nowrap' }} order={{ md: 1 }} lineHeight={2} fontSize='sm'>
+        <p>Type the message you want to share and click <strong>Encrypt</strong>.</p>
+        <p>We store the encrypted message and give you the encryption key.</p>
+        <Alert status='warning'>Nobody can read your message without the encryption key.</Alert>
+      </Box>
+      {formik.isSubmitting ? (
+        <Stack align='center' alignSelf='center' w='full'>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </Stack>
+      ) : (
+        <form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
+          <Flex w='full' direction='column' mt={{ base: 5, md: 0 }}>
+            <FormControl id='message' h='full' as={Flex} flexDirection='column'>
+              <FormLabel>Message</FormLabel>
+              <Textarea onChange={formik.handleChange} value={formik.values.message} resize='vertical' h='full' minH={200} />
+            </FormControl>
+            <HStack justify='flex-end' paddingTop={2} w='full' h={100}>
+              <Button type='submit'>Encrypt</Button>
+            </HStack>
+          </Flex>
+        </form>
       )}
     </Box>
-  )
+  );
 }
